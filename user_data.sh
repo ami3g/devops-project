8039c8f6fcd4c9981887db4ba9505b551c95eb2e
LENEY A. GANNASAN <amitesh3000@yahoo.com>
2025-08-27 22:40:44 -0400

    Updated URI

--- user_data.sh
+++ user_data.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Update and install necessary packages
+# Update the system and install Docker
 sudo yum update -y
 sudo yum install -y docker
 
-# Start the Docker service
+# Start and enable the Docker service
 sudo systemctl start docker
+sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
-newgrp docker
 
-# Login to ECR
-# Use the IAM role to get an ECR authentication token and log in to the registry
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin <YOUR_AWS_ACCOUNT_ID>.dkr.ecr.us-east-1.amazonaws.com
+# Authenticate Docker with ECR
+# Use the IAM role to get an ECR authentication token and log in
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 370936874275.dkr.ecr.us-east-1.amazonaws.com
 
 # Set environment variables for the application
 export DB_PASSWORD="${db_password}"
 export DB_HOST="${db_endpoint}"
 
 # Run the Django application using Docker
-docker run -p 5000:5000 ${ecr_repo_url}:latest
+# The full ECR URI is required to pull from your private registry.
+sudo docker run -p 5000:5000 370936874275.dkr.ecr.us-east-1.amazonaws.com/devops-project-app:latest
